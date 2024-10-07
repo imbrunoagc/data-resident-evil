d04429009b087a6451c00374920222acf469012e
Bruno Augusto <120321738+imbrunoagc@users.noreply.github.com>
2024-10-06 21:55:02 -0300

    feat: test in execution of schema from pydantic

---
+++ resident_evil_request/test.py
@@ -0,0 +1,23 @@
+from pydantic import ValidationError
+import json
+from schemas import CharactersPerson
+
+if __name__ == '__main__':
+    # Carregando o arquivo JSON
+    try:
+        with open('data/raw/basic_information_characters.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        # por item da lista, validar o schema 
+        for character_data in data:
+            try:
+                character = CharactersPerson(**character_data)
+                #print(character)
+            except ValidationError as e:
+                print(f"Erro de validação para {character_data['name']}: {e}")
+        print("\nSucesso na leitura utilizando o schema pydantic.")
+
+    except FileNotFoundError:
+        print("Arquivo JSON não encontrado.")
+    except json.JSONDecodeError:
+        print("Erro ao decodificar o arquivo JSON.")
