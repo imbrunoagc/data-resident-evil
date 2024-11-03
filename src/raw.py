7e3e1bd94c090587d8e5fa39dc30358fd674bcea
imbrunoagc <brunoaugusto864@gmail.com>
2024-11-03 17:18:29 -0300

    remove: remocao de arquivos

--- src/raw.py
+++
@@ -1,7 +0,0 @@
-from scrapy.collect import collect
-from scrapy.paramns import COOKIES, HEADERS
-
-print(HEADERS, COOKIES)
-
-FILE_PATH = './data/raw/raw_characters.json'
-collect(COOKIES, HEADERS).run_collect(FILE_PATH)
