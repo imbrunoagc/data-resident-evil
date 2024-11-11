8a46f7f7a20a903b60b4b347f826eb7bc551fa6a
imbrunoagc <brunoaugusto864@gmail.com>
2024-11-10 22:08:21 -0300

    feat: modulo de transformacoes auxiliares da camada silver

---
+++ src/tools/transform.py
@@ -0,0 +1,39 @@
+import re
+from datetime import datetime
+
+def extract_year(value) -> str:
+    if isinstance(value, str):
+        year = value.strip()[:4]
+        if year.isdigit():
+            year = int(year)
+            current_year = datetime.now().year
+            if 1900 <= year <= current_year:
+                return str(year)
+    return 'Desconhecido'
+
+def extract_type_sanguine(value: str) -> str:
+    if isinstance(value, str):
+        value = value.strip().lower()
+        map = {
+            '\xa0desconhecido.': 'Desconhecido',
+            'desconhecido.': 'Desconhecido',
+            'desconhecido': 'Desconhecido',
+            'desconhecido': 'Desconhecido'
+        }
+        return map.get(value, value.upper())
+    return 'Desconhecido'
+
+def extract_height(value) -> str:
+    if isinstance(value, str):  # Verifica se o valor é uma string
+        match = re.search(r'\b\d+,\d+(?:m|cm)\b', value) # Busca o primeiro padrão que corresponde a altura (ex: 1,75m ou 1,75cm)
+
+        if match:
+            return match.group(0)
+    return "Desconhecido"
+
+def extract_weight(value) -> str:
+    if isinstance(value, str):     # Verifica se o valor é uma string
+        match = re.search(r'\b\d+(?:,\d+)?kg\b', value)  # Busca o primeiro padrão que corresponde ao peso (ex: 84,5kg ou 84kg)
+        if match:
+            return match.group(0)
+    return "Desconhecido"
