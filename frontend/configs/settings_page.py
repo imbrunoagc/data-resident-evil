1e62c7162fe72eb386c6e147e231a92a2d07dbd6
imbrunoagc <brunoaugusto864@gmail.com>
2024-11-17 15:31:44 -0300

    fix: ajuste de emoji no sidebar da pagina

--- frontend/configs/settings_page.py
+++ frontend/configs/settings_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 def setup_page():
     st.set_page_config(
         page_title="Resident-Evil",
-        page_icon="☠️",
+        page_icon="🎯",
         layout="wide",
         initial_sidebar_state="expanded"
     )
