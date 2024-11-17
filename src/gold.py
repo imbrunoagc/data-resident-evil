aada63c3b1791cc24c67fa378cf5b96ea0e47081
imbrunoagc <brunoaugusto864@gmail.com>
2024-11-17 14:40:09 -0300

    fix: ajustes nos arquivos agregados

--- src/gold.py
+++ src/gold.py
@@ -10,15 +10,14 @@ from tools.rules_gold import (
     blood_type_distribution,
     explode_dataframe,
     top_10_characters_with_most_appearances,
-    top_10_most_popular_characters,
+    top_10_most_popular_appearances,
 )
-
 
 class ResidentEvil_Silver_to_Gold:
     def __init__(self) -> None:
         self.client = boto3.client(
                 's3',
-                endpoint_url='http://localhost:9000', #os.environ.get('ENDPOINT'),  # Correct API port
+                endpoint_url=os.environ.get('ENDPOINT'), #'http://localhost:9000' # Correct API port
                 aws_access_key_id=os.environ.get('ACCESS_KEY'),
                 aws_secret_access_key=os.environ.get('SECRET_KEY'),
                 region_name='us-east-1'
@@ -39,10 +38,26 @@ if __name__ == "__main__":
     cls_gold = ResidentEvil_Silver_to_Gold()
     data = cls_gold._get_parquet_s3()
     
-    df_exploded = explode_dataframe(data, 'aparicoes') #exploded data
-    df_exploded.to_parquet('./data/gold_persist/characters_exploded.parquet', engine='pyarrow')
+    # Exploded data
+    df_exploded = explode_dataframe(data, 'aparicoes')
+    df_top_10 = top_10_characters_with_most_appearances(df_exploded)
+    df_top_10_appearances = top_10_most_popular_appearances(df_exploded)
+    df_blood_type_distribution = blood_type_distribution(data)
+    df_average_by_blood_type = average_height_and_weight_by_blood_type(data)
+    
+    # Save Local
+    df_exploded.to_parquet('./data/characters_exploded.parquet', engine='pyarrow')
+    df_top_10.to_parquet('./data/top_10_characters_with_most_appearances.parquet', engine='pyarrow')
+    df_top_10_appearances.to_parquet('./data/top_10_appearances.parquet', engine='pyarrow')
+    df_blood_type_distribution.to_parquet('./data/blood_type_distribution.parquet', engine='pyarrow')
+    df_average_by_blood_type.to_parquet('./data/average_by_blood_type.parquet', engine='pyarrow')
+    
+    # Load Layer Gold in S3
     cls_gold.build_and_loadS3(df_exploded, 'characters_exploded')
-    cls_gold.build_and_loadS3(top_10_most_popular_characters(df_exploded), 'top_10_most_popular_characters')
-    cls_gold.build_and_loadS3(top_10_characters_with_most_appearances(df_exploded), 'top_10_characters_with_most_appearances')
-    cls_gold.build_and_loadS3(blood_type_distribution(data), 'blood_type_distribution')
-    cls_gold.build_and_loadS3(average_height_and_weight_by_blood_type(data), 'average_by_blood_type')
+    cls_gold.build_and_loadS3(df_top_10_appearances, 'top_10_most_popular_appearances')
+    cls_gold.build_and_loadS3(df_top_10, 'top_10_characters_with_most_appearances')
+    cls_gold.build_and_loadS3(df_blood_type_distribution, 'blood_type_distribution')
+    cls_gold.build_and_loadS3(df_average_by_blood_type, 'average_by_blood_type')
+    
+    
+    print("### Finished the Gold.py Module")
