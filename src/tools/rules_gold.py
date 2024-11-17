8bb2afe65f321a0b40e48239f882f9fbe5e50dd6
imbrunoagc <brunoaugusto864@gmail.com>
2024-11-17 15:06:15 -0300

    fix: comentando os prints interno de funcoes

--- src/tools/rules_gold.py
+++ src/tools/rules_gold.py
@@ -16,8 +16,8 @@ def blood_type_distribution(data: pd.DataFrame) -> pd.DataFrame:
     Contagem de personagens por tipo sanguíneo
     """
     blood_type_count = data["tipo_sanguineo"].value_counts().reset_index()
-    print("Distribuição por Tipo Sanguíneo:")
-    print(blood_type_count)
+    #print("Distribuição por Tipo Sanguíneo:")
+    #print(blood_type_count)
     return blood_type_count
 
 def average_height_and_weight_by_blood_type(df: pd.DataFrame) -> pd.DataFrame:
@@ -25,8 +25,8 @@ def average_height_and_weight_by_blood_type(df: pd.DataFrame) -> pd.DataFrame:
     Obter Média de Altura por Tipo Sanguíneo
     """
     mean_physics = df.groupby("tipo_sanguineo")[["altura", "peso"]].mean().reset_index()
-    print("Média de Altura e Peso por Tipo Sanguíneo:")
-    print(mean_physics)
+    #print("Média de Altura e Peso por Tipo Sanguíneo:")
+    #print(mean_physics)
     return mean_physics
 
 def top_10_characters_with_most_appearances(df_exploded: pd.DataFrame) -> pd.DataFrame:
