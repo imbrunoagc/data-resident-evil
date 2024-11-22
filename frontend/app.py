1069afdc7a590995966c3d1b9e1253d03e16655e
imbrunoagc <brunoaugusto864@gmail.com>
2024-11-22 08:04:37 -0300

    fix: atualizacao do app-streamlit para apenas leitura do arquivo persistido local

--- frontend/app.py
+++ frontend/app.py
@@ -1,9 +1,13 @@
+import os
 import streamlit as st
 import pandas as pd
-from frontend.configs.settings_page import setup_page
-from frontend.components.footer import footer
-from frontend.components.input_css import styling
-from frontend.resources.readFile import readFile
+from configs.settings_page import setup_page
+from components.footer import footer
+from components.input_css import styling
+
+dir_current = os.path.abspath(os.path.dirname(__file__))
+path_raiz = os.path.join(dir_current, "..")
+path_data = os.path.join(path_raiz, "data")
 
 setup_page() # Page configuration
 
@@ -13,35 +17,29 @@ LOCAL = True
 
 if LOCAL: # Local
     df = pd.read_parquet(
-        'data/characters_exploded.parquet',
+        f'{path_data}/characters_exploded.parquet',
         engine='pyarrow'
         )
     
     df_top_10_characters_with_most_appearances = pd.read_parquet(
-        'data/top_10_characters_with_most_appearances.parquet',
+        f'{path_data}/top_10_characters_with_most_appearances.parquet',
         engine='pyarrow'
         )
     
     df_top_10_appearances = pd.read_parquet(
-        'data/top_10_appearances.parquet',
+        f'{path_data}/top_10_appearances.parquet',
         engine='pyarrow'
     )
     
     df_average_by_blood_type = pd.read_parquet(
-        'data/average_by_blood_type.parquet',
+        f'{path_data}/average_by_blood_type.parquet',
         engine='pyarrow'
     )
     
     df_blood_type_distribution = pd.read_parquet(
-        'data/blood_type_distribution.parquet',
+        f'{path_data}/blood_type_distribution.parquet',
         engine='pyarrow'
     )   
-    
-else: # S3
-    df = readFile('characters_exploded')
-    df_top_10_characters_with_most_appearances = readFile('top_10_characters_with_most_appearances')
-    df_average_by_blood_type = readFile('average_by_blood_type')
-    df_blood_type_distribution = readFile('blood_type_distribution')
 
 col1, col2, col3 = st.columns([1, 3, 1]) # Set 3 columns
 
